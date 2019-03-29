68d4d52a842ffc6910db96b83902a42f376f2fee
Andreas Lundell <andreas.lundell@abo.fi>
2019-03-29 23:57:26 +0200

    Remove Visual studio files

--- MSVisualStudio/converttoansi.ps1
+++
@@ -1,8 +0,0 @@
-$files = @("..\src\SHOT.cpp","..\src\Tasks\TaskPrintIterationHeader.cpp", "..\src\Tasks\TaskPrintProblemStats.cpp", "..\src\Tasks\TaskPrintSolution.cpp")
-foreach ($file in $files) 
-{
-    Copy-Item $file ([io.path]::ChangeExtension($file, '.bak')) 
-    Get-Content $file -Encoding utf8 | Out-File temp.cpp -Encoding Oem
-    Move-Item temp.cpp $file -force
-}
-
