212e8d2781f87d1951d03df2f681155b7a01d27f
Andreas Lundell <andreas.lundell@abo.fi>
2019-05-01 23:22:42 +0300

    Update command line program

--- src/SHOT.cpp
+++ src/SHOT.cpp
@@ -16,6 +16,8 @@
 #include "Settings.h"
 #include "Problem.h"
 
+#include "argh.h"
+
 #include <filesystem>
 #include <iostream>
 #include <memory>
@@ -28,127 +30,177 @@ int main(int argc, char* argv[])
     std::unique_ptr<Solver> solver = std::make_unique<Solver>();
     auto env = solver->getEnvironment();
 
-    if(argc == 1)
+    argh::parser cmdl;
+    cmdl.add_params({ "--opt", "--osol" });
+    cmdl.add_params({ "--osrl", "--trc" });
+
+    cmdl.parse(argc, argv);
+
+    env->report->outputSolverHeader();
+
+    if(cmdl["--help"])
     {
-        env->report->outputSolverHeader();
-        std::cout << " Usage: filename.[osil|xml|gms|nl] options.[opt|xml|osol] results.osrl results.trc" << std::endl;
+
+        env->output->outputInfo("╶──────────────────────────────────────────────────────────────────────────────────"
+                                "───────────────────────────────────╴\r\n");
+
+        env->output->outputCritical("  Usage: SHOT PROBLEMFILE [OPTIONS]");
+        env->output->outputCritical("");
+        env->output->outputCritical("  SHOT has been compiled with support for the following problem formats ");
+#ifdef HAS_OS
+        env->output->outputCritical("   OSiL (.osil or .xml) ");
+        env->output->outputCritical("   AMPL (.nl) ");
+#endif
+#ifdef HAS_GAMS
+        env->output->outputCritical("   GAMS (.gms) ");
+#endif
+        env->output->outputCritical("");
+        env->output->outputCritical("  The following command line options can also be used:");
+        env->output->outputCritical("");
+        env->output->outputCritical("   --opt [FILE]           Reads in options from FILE in GAMS format");
+        env->output->outputCritical("                          If FILE is empty, a new options file will be created");
+        env->output->outputCritical("   --osol [FILE]          Reads in options from FILE in OSoL format");
+        env->output->outputCritical("                          If FILE is empty, a new options file will be created");
+        env->output->outputCritical("   --osrl FILE            Sets the filename for the OSrL result file");
+        env->output->outputCritical("   --trc FILE             Sets the filename for the GAMS trace file");
 
         return (0);
     }
 
-    bool defaultOptionsGenerated = false;
-
+    std::string filename;
     std::filesystem::path resultFile, optionsFile, traceFile;
 
-    if(argc == 2) // No options file specified, use or create defaults
+    // Read or create options file
+
+    bool defaultOptionsGenerated = false;
+
+    if(cmdl("--opt") >> filename) // Have specified a opt-file
     {
 
-        auto optFilePath = std::filesystem::current_path() / std::filesystem::path("options.opt");
-        auto osilFilePath = std::filesystem::current_path() / std::filesystem::path("options.xml");
+        auto filepath = std::filesystem::current_path() / std::filesystem::path(filename);
 
-        bool GAMSOptFileExists = std::filesystem::exists(optFilePath);
-        bool OSoLFileExists = std::filesystem::exists(osilFilePath);
-
-        if(GAMSOptFileExists)
+        if(std::filesystem::exists(filepath))
         {
-            optionsFile = optFilePath;
-        }
-        else if(OSoLFileExists)
-        {
-            optionsFile = osilFilePath;
+            optionsFile = filepath;
         }
         else
         {
-            // Create OSoL-file
-            if(!Utilities::writeStringToFile(osilFilePath, solver->getOptionsOSoL()))
-                env->output->outputError(" Error when writing OSoL file: " + osilFilePath.string());
+            env->output->outputCritical("  Options file not found: " + filepath.string());
+            return 0;
+        }
+    }
+    else if(cmdl["--opt"]) // Create a new opt-file
+    {
+        auto filepath = std::filesystem::current_path() / std::filesystem::path("options.opt");
 
-            // Create GAMS option file
-            if(!Utilities::writeStringToFile(optFilePath, solver->getOptions()))
-                env->output->outputError(" Error when writing options file: " + optFilePath.string());
+        if(std::filesystem::exists(filepath))
+        {
+            optionsFile = filepath;
+            env->output->outputInfo("  Default options file written to: " + filepath.string());
+        }
+        else
+        {
+            // Create option file
+            if(!Utilities::writeStringToFile(filepath, solver->getOptions()))
+            {
+                env->output->outputCritical("  Error when writing options file: " + filepath.string());
+                return 0;
+            }
 
             defaultOptionsGenerated = true;
         }
     }
-    else if(argc == 3)
+    else if(cmdl("--osol") >> filename) // Have specified a OSoL-file
     {
-        if(!std::filesystem::exists(argv[2]))
+        auto filepath = std::filesystem::current_path() / std::filesystem::path(filename);
+
+        if(std::filesystem::exists(filepath))
         {
-            env->report->outputSolverHeader();
-
-            return (0);
+            optionsFile = filepath;
+            env->output->outputInfo("  Default options file written to: " + filepath.string());
         }
-
-        optionsFile = std::filesystem::path(argv[2]);
+        else
+        {
+            env->output->outputCritical(" Options file not found: " + filepath.string());
+            return 0;
+        }
     }
-    else if(argc == 4)
+    else if(cmdl["--osol"]) // Create a new OSoL-file
     {
-        if(!std::filesystem::exists(argv[2]))
-        {
-            env->report->outputSolverHeader();
-            std::cout << " Options file " << argv[2] << " not found!" << std::endl;
+        auto filepath = std::filesystem::current_path() / std::filesystem::path("options.xml");
 
-            return (0);
+        if(std::filesystem::exists(filepath))
+        {
+            optionsFile = filepath;
         }
-
-        optionsFile = std::filesystem::path(argv[2]);
-        resultFile = std::filesystem::path(argv[3]);
-    }
-    else
-    {
-        if(!std::filesystem::exists(argv[2]))
+        else
         {
-            env->report->outputSolverHeader();
-            std::cout << " Options file " << argv[2] << " not found!" << std::endl;
-
-            return (0);
-        }
-
-        optionsFile = std::filesystem::path(argv[2]);
-        resultFile = std::filesystem::path(argv[3]);
-        traceFile = std::filesystem::path(argv[4]);
-    }
-
-    try
-    {
-        if(!std::filesystem::exists(argv[1]))
-        {
-            env->report->outputSolverHeader();
-            std::cout << " Problem file " << argv[1] << " not found!" << std::endl;
-
-            return (0);
-        }
-
-        std::string fileName = argv[1];
-
-        if(!defaultOptionsGenerated)
-        {
-            if(!optionsFile.empty() && !solver->setOptionsFromFile(optionsFile.string()))
+            // Create OSoL-file
+            if(!Utilities::writeStringToFile(filepath, solver->getOptionsOSoL()))
             {
-                env->report->outputSolverHeader();
-                std::cout << " Cannot set options!" << std::endl;
-                return (0);
+                env->output->outputCritical("  Error when writing OSoL file: " + filepath.string());
+                return 0;
             }
+        }
+
+        defaultOptionsGenerated = true;
+    }
+
+    if(!defaultOptionsGenerated)
+    {
+        if(!optionsFile.empty() && !solver->setOptionsFromFile(optionsFile.string()))
+        {
+            env->output->outputCritical("  Cannot set options from file: " + optionsFile.string());
+            return (0);
         }
 
         env->output->setLogLevels(static_cast<E_LogLevel>(env->settings->getSetting<int>("Console.LogLevel", "Output")),
             static_cast<E_LogLevel>(env->settings->getSetting<int>("File.LogLevel", "Output")));
-        env->report->outputSolverHeader();
+    }
 
-        if(!solver->setProblem(fileName))
+    // Define result file locations
+
+    if(cmdl("--osrl") >> filename) // Have specified a OSrL-file
+    {
+        resultFile = std::filesystem::current_path() / std::filesystem::path(filename);
+    }
+
+    if(cmdl("--trc") >> filename) // Have specified a trace-file
+    {
+        traceFile = std::filesystem::current_path() / std::filesystem::path(filename);
+        std::cout << traceFile << std::endl;
+    }
+
+    // Read problem file
+
+    if(!cmdl(1) || !(cmdl(1) >> filename))
+    {
+        env->output->outputCritical("  No problem file specified.");
+        env->output->outputCritical("  Try 'SHOT --help' for more information.");
+        return (0);
+    }
+
+    if(!std::filesystem::exists(filename))
+    {
+
+        env->output->outputCritical("   Problem file " + filename + " not found!");
+        return (0);
+    }
+
+    if(!solver->setProblem(filename))
+    {
+        env->output->outputCritical("   Error when reading problem file.");
+        return (0);
+    }
+
+    env->report->outputOptionsReport();
+    env->report->outputProblemInstanceReport();
+
+    try
+    {
+        if(!solver->solveProblem()) // Solve the problem
         {
-            env->output->outputError(" Error when reading problem file.");
-
-            return (0);
-        }
-
-        env->report->outputOptionsReport();
-        env->report->outputProblemInstanceReport();
-
-        if(!solver->solveProblem()) // solve problem
-        {
-            env->output->outputError(" Error when solving problem.");
-
+            env->output->outputCritical(" Error when solving problem.");
             return (0);
         }
 
@@ -157,10 +209,9 @@ int main(int argc, char* argv[])
         env->output->outputInfo("╶──────────────────────────────────────────────────────────────────────────────────"
                                 "───────────────────────────────────╴\r\n");
     }
-    catch(const Error& eclass)
+    catch(const std::exception& error)
     {
-        env->output->outputError(eclass.message);
-
+        env->output->outputError(" Error when solving problem", error.what());
         return (0);
     }
 
@@ -171,21 +222,18 @@ int main(int argc, char* argv[])
         std::filesystem::path resultPath(env->settings->getSetting<std::string>("ResultPath", "Output"));
         resultPath /= env->problem->name;
         resultPath = resultPath.replace_extension(".osrl");
-        env->output->outputInfo(" Results written to: " + resultPath.string());
 
         if(!Utilities::writeStringToFile(resultPath.string(), osrl))
-        {
-            env->output->outputError(" Error when writing OSrL file: " + resultPath.string());
-        }
+            env->output->outputCritical(" Error when writing OSrL file to: " + resultPath.string());
+        else
+            env->output->outputInfo(" Results written to: " + resultPath.string());
     }
     else
     {
-        env->output->outputInfo(" Results written to: " + resultFile.string());
-
         if(!Utilities::writeStringToFile(resultFile.string(), osrl))
-        {
-            env->output->outputError(" Error when writing OSrL file: " + resultFile.string());
-        }
+            env->output->outputCritical(" Error when writing OSrL file to: " + resultFile.string());
+        else
+            env->output->outputInfo(" Results written to: " + resultFile.string());
     }
 
     std::string trace = solver->getResultsTrace();
@@ -195,19 +243,18 @@ int main(int argc, char* argv[])
         std::filesystem::path tracePath(env->settings->getSetting<std::string>("ResultPath", "Output"));
         tracePath /= env->problem->name;
         tracePath = tracePath.replace_extension(".trc");
-        env->output->outputInfo("                     " + tracePath.string());
 
         if(!Utilities::writeStringToFile(tracePath.string(), trace))
-        {
-            env->output->outputError(" Error when writing trace file: " + tracePath.string());
-        }
+            env->output->outputCritical(" Error when writing trace file: " + tracePath.string());
+        else
+            env->output->outputInfo("                     " + tracePath.string());
     }
     else
     {
         if(!Utilities::writeStringToFile(traceFile.string(), trace))
-        {
-            env->output->outputError(" Error when writing trace file: " + traceFile.string());
-        }
+            env->output->outputCritical(" Error when writing trace file: " + traceFile.string());
+        else
+            env->output->outputInfo("                     " + traceFile.string());
     }
 
     return (0);
