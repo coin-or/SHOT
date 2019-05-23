276cbdfee3db5e02c3cba20cfd7757a60bbc7699
Andreas Lundell <andreas.lundell@abo.fi>
2019-05-23 20:57:23 +0300

    Change output of warning for no interior point

--- src/Tasks/TaskSelectHyperplanePointsESH.cpp
+++ src/Tasks/TaskSelectHyperplanePointsESH.cpp
@@ -51,7 +51,7 @@ void TaskSelectHyperplanePointsESH::run(std::vector<SolutionPoint> solPoints)
             hyperplaneSolutionPointStrategyInitialized = true;
         }
 
-        env->output->outputWarning("        Adding cutting plane since no interior point is known.");
+        env->output->outputDebug("        Adding cutting plane since no interior point is known.");
         tSelectHPPts->run(solPoints);
 
         env->timing->stopTimer("DualCutGenerationRootSearch");
