d91a0ca179f56ff2c9bcb11a50b7cffeb27ee3d5
Andreas Lundell <andreas.lundell@abo.fi>
2019-06-26 14:30:41 +0300

    Format CMake files using cmake_format

--- misc/FindCPLEX.cmake
+++ misc/FindCPLEX.cmake
@@ -1,81 +1,75 @@
-# Try to find the CPLEX, Concert and IloCplex libraries.
-# Modified from https://github.com/ampl/mp/blob/master/support/cmake/FindCPLEX.cmake
+# Try to find the CPLEX, Concert and IloCplex libraries. Modified from
+# https://github.com/ampl/mp/blob/master/support/cmake/FindCPLEX.cmake
 
 include(FindPackageHandleStandardArgs)
 
-# Find the path to CPLEX Studio.
-# CPLEX Studio 12.4 can be installed in the following default locations:
-#   /opt/ibm/ILOG/CPLEX_Studio<edition>124 - Linux
-#   /opt/IBM/ILOG/CPLEX_Studio<edition>124 - UNIX
-#   ~/Applications/IBM/ILOG/CPLEX_Studio<edition>124 - Mac OS X
-#   C:\Program Files\IBM\ILOG\CPLEX_Studio<edition>124 - Windows
-if (UNIX)
+# Find the path to CPLEX Studio. CPLEX Studio 12.4 can be installed in the following default locations:
+# /opt/ibm/ILOG/CPLEX_Studio<edition>124 - Linux /opt/IBM/ILOG/CPLEX_Studio<edition>124 - UNIX
+# ~/Applications/IBM/ILOG/CPLEX_Studio<edition>124 - Mac OS X C:\Program Files\IBM\ILOG\CPLEX_Studio<edition>124 -
+# Windows
+if(UNIX)
   set(CPLEX_ILOG_DIRS /opt/ibm/ILOG /opt/IBM/ILOG ${CPLEX_DIR})
-  if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+  if(CMAKE_SIZEOF_VOID_P EQUAL 8)
     set(CPLEX_ARCH x86-64)
-  else ()
+  else()
     set(CPLEX_ARCH x86)
-  endif ()
-  if (APPLE)
+  endif()
+  if(APPLE)
     set(CPLEX_ILOG_DIRS $ENV{HOME}/Applications/IBM/ILOG ${CPLEX_ILOG_DIRS})
-    foreach (suffix "osx" "darwin9_gcc4.0")
-      set(CPLEX_LIB_PATH_SUFFIXES
-          ${CPLEX_LIB_PATH_SUFFIXES} lib/${CPLEX_ARCH}_${suffix}/static_pic)
-    endforeach ()
-  else ()
-    set(CPLEX_LIB_PATH_SUFFIXES
-      lib/${CPLEX_ARCH}_sles10_4.1/static_pic lib/${CPLEX_ARCH}_linux/static_pic)
-  endif ()
-else ()
+    foreach(suffix "osx" "darwin9_gcc4.0")
+      set(CPLEX_LIB_PATH_SUFFIXES ${CPLEX_LIB_PATH_SUFFIXES} lib/${CPLEX_ARCH}_${suffix}/static_pic)
+    endforeach()
+  else()
+    set(CPLEX_LIB_PATH_SUFFIXES lib/${CPLEX_ARCH}_sles10_4.1/static_pic lib/${CPLEX_ARCH}_linux/static_pic)
+  endif()
+else()
   set(CPLEX_ILOG_DIRS "C:/Program Files/IBM/ILOG")
-  if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+  if(CMAKE_SIZEOF_VOID_P EQUAL 8)
     set(CPLEX_ARCH x64)
-  else ()
+  else()
     set(CPLEX_ARCH x86)
     set(CPLEX_ILOG_DIRS "C:/Program Files (x86)/IBM/ILOG" ${CPLEX_ILOG_DIRS})
-  endif ()
-   # Amended for VS and its various toolsets
-  # https://cmake.org/cmake/help/v3.11/variable/MSVC_VERSION.html
-  # Can use GREATER_EQUAL instead of the mess below if cmake version >= 3.7
+  endif()
+  # Amended for VS and its various toolsets https://cmake.org/cmake/help/v3.11/variable/MSVC_VERSION.html Can use
+  # GREATER_EQUAL instead of the mess below if cmake version >= 3.7
   if(NOT (MSVC_VERSION LESS 1910))
-	set(CPLEX_LIB_PATH_SUFFIXES lib/${CPLEX_ARCH}_windows_vs2017/stat_mda)
+    set(CPLEX_LIB_PATH_SUFFIXES lib/${CPLEX_ARCH}_windows_vs2017/stat_mda)
     set(CPLEX_LIB_PATH_SUFFIXES_DEBUG lib/${CPLEX_ARCH}_windows_vs2017/stat_mdd)
-  elseif (NOT (MSVC_VERSION LESS 1900)) # to support VS2015 with 2013 libraries change below
+  elseif(NOT (MSVC_VERSION LESS 1900)) # to support VS2015 with 2013 libraries change below
     set(CPLEX_LIB_PATH_SUFFIXES lib/${CPLEX_ARCH}_windows_vs2015/stat_mda)
     set(CPLEX_LIB_PATH_SUFFIXES_DEBUG lib/${CPLEX_ARCH}_windows_vs2015/stat_mdd)
-  elseif (NOT (MSVC_VERSION LESS 1800))
-  set(CPLEX_LIB_PATH_SUFFIXES lib/${CPLEX_ARCH}_windows_vs2013/stat_mda)
+  elseif(NOT (MSVC_VERSION LESS 1800))
+    set(CPLEX_LIB_PATH_SUFFIXES lib/${CPLEX_ARCH}_windows_vs2013/stat_mda)
     set(CPLEX_LIB_PATH_SUFFIXES_DEBUG lib/${CPLEX_ARCH}_windows_vs2013/stat_mdd)
-  elseif (NOT (MSVC_VERSION LESS 1700))
+  elseif(NOT (MSVC_VERSION LESS 1700))
     set(CPLEX_LIB_PATH_SUFFIXES lib/${CPLEX_ARCH}_windows_vs2012/stat_mda)
     set(CPLEX_LIB_PATH_SUFFIXES_DEBUG lib/${CPLEX_ARCH}_windows_vs2012/stat_mdd)
-  elseif (NOT (MSVC_VERSION LESS 1600))
+  elseif(NOT (MSVC_VERSION LESS 1600))
     set(CPLEX_LIB_PATH_SUFFIXES lib/${CPLEX_ARCH}_windows_vs2010/stat_mda)
     set(CPLEX_LIB_PATH_SUFFIXES_DEBUG lib/${CPLEX_ARCH}_windows_vs2010/stat_mdd)
-   elseif (NOT (MSVC_VERSION LESS 1500))
+  elseif(NOT (MSVC_VERSION LESS 1500))
     set(CPLEX_LIB_PATH_SUFFIXES lib/${CPLEX_ARCH}_windows_vs2008/stat_mda)
     set(CPLEX_LIB_PATH_SUFFIXES_DEBUG lib/${CPLEX_ARCH}_windows_vs2008/stat_mdd)
-  endif ()
-endif ()
+  endif()
+endif()
 
-if (NOT CPLEX_STUDIO_DIR)
-  foreach (dir ${CPLEX_ILOG_DIRS})
+if(NOT CPLEX_STUDIO_DIR)
+  foreach(dir ${CPLEX_ILOG_DIRS})
     file(GLOB CPLEX_STUDIO_DIRS "${dir}/CPLEX_Studio*")
     list(SORT CPLEX_STUDIO_DIRS)
     list(REVERSE CPLEX_STUDIO_DIRS)
-    if (CPLEX_STUDIO_DIRS)
+    if(CPLEX_STUDIO_DIRS)
       list(GET CPLEX_STUDIO_DIRS 0 CPLEX_STUDIO_DIR_)
       message(STATUS "Found CPLEX Studio: ${CPLEX_STUDIO_DIR_}")
-      break ()
-    endif ()
-  endforeach ()
-  
-  if (NOT CPLEX_STUDIO_DIR_)
+      break()
+    endif()
+  endforeach()
+
+  if(NOT CPLEX_STUDIO_DIR_)
     set(CPLEX_STUDIO_DIR_ CPLEX_STUDIO_DIR-NOTFOUND)
-  endif ()
-  set(CPLEX_STUDIO_DIR ${CPLEX_STUDIO_DIR_} CACHE PATH
-    "Path to the CPLEX Studio directory")
-endif ()
+  endif()
+  set(CPLEX_STUDIO_DIR ${CPLEX_STUDIO_DIR_} CACHE PATH "Path to the CPLEX Studio directory")
+endif()
 
 find_package(Threads)
 
@@ -88,106 +82,103 @@ set(CPLEX_DIR ${CPLEX_STUDIO_DIR_}/cplex)
 find_path(CPLEX_INCLUDE_DIR ilcplex/cplex.h PATHS ${CPLEX_DIR}/include)
 
 macro(find_win_cplex_library var path_suffixes)
-  foreach (s ${path_suffixes})
-	file(GLOB CPLEX_LIBRARY_CANDIDATES "${CPLEX_DIR}/${s}/cplex*.lib")
-    if (CPLEX_LIBRARY_CANDIDATES)
+  foreach(s ${path_suffixes})
+    file(GLOB CPLEX_LIBRARY_CANDIDATES "${CPLEX_DIR}/${s}/cplex*.lib")
+    if(CPLEX_LIBRARY_CANDIDATES)
       list(GET CPLEX_LIBRARY_CANDIDATES 0 ${var})
-      break ()
-    endif ()
-  endforeach ()
-  if (NOT ${var})
+      break()
+    endif()
+  endforeach()
+  if(NOT ${var})
     set(${var} NOTFOUND)
-  endif ()
+  endif()
 endmacro()
 
 # Find the CPLEX library.
-if (UNIX)
-  find_library(CPLEX_LIBRARY NAMES cplex
-    PATHS ${CPLEX_DIR} PATH_SUFFIXES ${CPLEX_LIB_PATH_SUFFIXES})
+if(UNIX)
+  find_library(CPLEX_LIBRARY NAMES cplex PATHS ${CPLEX_DIR} PATH_SUFFIXES ${CPLEX_LIB_PATH_SUFFIXES})
   set(CPLEX_LIBRARY_DEBUG ${CPLEX_LIBRARY})
-elseif (NOT CPLEX_LIBRARY)
-#message("In windows trying " ${${CPLEX_LIB_PATH_SUFFIXES}})
-  # On Windows the version is appended to the library name which cannot be
-  # handled by find_library, so search manually.
+elseif(NOT CPLEX_LIBRARY)
+  # message("In windows trying " ${${CPLEX_LIB_PATH_SUFFIXES}}) On Windows the version is appended to the library name
+  # which cannot be handled by find_library, so search manually.
   find_win_cplex_library(CPLEX_LIB "${CPLEX_LIB_PATH_SUFFIXES}")
-  #message(" CPLEX_LIB " ${CPLEX_LIB})
-  #message(" CPLEX_LIBRARY " ${CPLEX_LIBRARY})
+  # message(" CPLEX_LIB " ${CPLEX_LIB}) message(" CPLEX_LIBRARY " ${CPLEX_LIBRARY})
   set(CPLEX_LIBRARY ${CPLEX_LIB} CACHE FILEPATH "Path to the CPLEX library")
   find_win_cplex_library(CPLEX_LIB "${CPLEX_LIB_PATH_SUFFIXES_DEBUG}")
-    
-  set(CPLEX_LIBRARY_DEBUG ${CPLEX_LIB} CACHE
-    FILEPATH "Path to the debug CPLEX library")
-	#message(" CPLEX_LIBRARY " ${CPLEX_LIBRARY})
-  if (CPLEX_LIBRARY MATCHES ".*/(cplex.*)\\.lib")
+
+  set(CPLEX_LIBRARY_DEBUG ${CPLEX_LIB} CACHE FILEPATH "Path to the debug CPLEX library")
+  # message(" CPLEX_LIBRARY " ${CPLEX_LIBRARY})
+  if(CPLEX_LIBRARY MATCHES ".*/(cplex.*)\\.lib")
     file(GLOB CPLEX_DLL_ "${CPLEX_DIR}/bin/*/${CMAKE_MATCH_1}.dll")
     set(CPLEX_DLL ${CPLEX_DLL_} CACHE PATH "Path to the CPLEX DLL.")
-	#message(" CPLEX_DLL " ${CPLEX_DLL})
-  endif ()
-endif ()
+    # message(" CPLEX_DLL " ${CPLEX_DLL})
+  endif()
+endif()
 
-# Handle the QUIETLY and REQUIRED arguments and set CPLEX_FOUND to TRUE
-# if all listed variables are TRUE.
-find_package_handle_standard_args(
-  CPLEX DEFAULT_MSG CPLEX_LIBRARY CPLEX_LIBRARY_DEBUG CPLEX_INCLUDE_DIR)
+# Handle the QUIETLY and REQUIRED arguments and set CPLEX_FOUND to TRUE if all listed variables are TRUE.
+find_package_handle_standard_args(CPLEX
+                                  DEFAULT_MSG
+                                  CPLEX_LIBRARY
+                                  CPLEX_LIBRARY_DEBUG
+                                  CPLEX_INCLUDE_DIR)
 
 mark_as_advanced(CPLEX_LIBRARY CPLEX_LIBRARY_DEBUG CPLEX_INCLUDE_DIR)
 
 # ----------------------------------------------------------------------------
 # Concert
 
-macro(find_cplex_library var name paths)
-  find_library(${var} NAMES ${name}
-    PATHS ${paths} PATH_SUFFIXES ${CPLEX_LIB_PATH_SUFFIXES})
-  if (UNIX)
+macro(find_cplex_library
+      var
+      name
+      paths)
+  find_library(${var} NAMES ${name} PATHS ${paths} PATH_SUFFIXES ${CPLEX_LIB_PATH_SUFFIXES})
+  if(UNIX)
     set(${var}_DEBUG ${${var}})
-  else ()
-    find_library(${var}_DEBUG NAMES ${name}
-      PATHS ${paths} PATH_SUFFIXES ${CPLEX_LIB_PATH_SUFFIXES_DEBUG})
-  endif ()
+  else()
+    find_library(${var}_DEBUG NAMES ${name} PATHS ${paths} PATH_SUFFIXES ${CPLEX_LIB_PATH_SUFFIXES_DEBUG})
+  endif()
 endmacro()
 
 set(CPLEX_CONCERT_DIR ${CPLEX_STUDIO_DIR}/concert)
 
 # Find the Concert include directory.
-find_path(CPLEX_CONCERT_INCLUDE_DIR ilconcert/ilosys.h
-  PATHS ${CPLEX_CONCERT_DIR}/include)
+find_path(CPLEX_CONCERT_INCLUDE_DIR ilconcert/ilosys.h PATHS ${CPLEX_CONCERT_DIR}/include)
 
 # Find the Concert library.
 find_cplex_library(CPLEX_CONCERT_LIBRARY concert ${CPLEX_CONCERT_DIR})
 
-# Handle the QUIETLY and REQUIRED arguments and set CPLEX_CONCERT_FOUND to
-# TRUE if all listed variables are TRUE.
-find_package_handle_standard_args(
-  CPLEX_CONCERT DEFAULT_MSG CPLEX_CONCERT_LIBRARY CPLEX_CONCERT_LIBRARY_DEBUG
-  CPLEX_CONCERT_INCLUDE_DIR)
+# Handle the QUIETLY and REQUIRED arguments and set CPLEX_CONCERT_FOUND to TRUE if all listed variables are TRUE.
+find_package_handle_standard_args(CPLEX_CONCERT
+                                  DEFAULT_MSG
+                                  CPLEX_CONCERT_LIBRARY
+                                  CPLEX_CONCERT_LIBRARY_DEBUG
+                                  CPLEX_CONCERT_INCLUDE_DIR)
 
-mark_as_advanced(CPLEX_CONCERT_LIBRARY CPLEX_CONCERT_LIBRARY_DEBUG
-  CPLEX_CONCERT_INCLUDE_DIR)
+mark_as_advanced(CPLEX_CONCERT_LIBRARY CPLEX_CONCERT_LIBRARY_DEBUG CPLEX_CONCERT_INCLUDE_DIR)
 
 # ----------------------------------------------------------------------------
 # IloCplex - depends on CPLEX and Concert
 
 include(CheckCXXCompilerFlag)
 check_cxx_compiler_flag(-Wno-long-long HAVE_WNO_LONG_LONG_FLAG)
-if (HAVE_WNO_LONG_LONG_FLAG)
+if(HAVE_WNO_LONG_LONG_FLAG)
   # Required if -pedantic is used.
   set(CPLEX_ILOCPLEX_DEFINITIONS -Wno-long-long)
-endif ()
+endif()
 
-# Find the IloCplex include directory - normally the same as the one for CPLEX
-# but check if ilocplex.h is there anyway.
-find_path(CPLEX_ILOCPLEX_INCLUDE_DIR ilcplex/ilocplex.h
-  PATHS ${CPLEX_INCLUDE_DIR})
+# Find the IloCplex include directory - normally the same as the one for CPLEX but check if ilocplex.h is there anyway.
+find_path(CPLEX_ILOCPLEX_INCLUDE_DIR ilcplex/ilocplex.h PATHS ${CPLEX_INCLUDE_DIR})
 
 # Find the IloCplex library.
 find_cplex_library(CPLEX_ILOCPLEX_LIBRARY ilocplex ${CPLEX_DIR})
 
-# Handle the QUIETLY and REQUIRED arguments and set CPLEX_ILOCPLEX_FOUND to
-# TRUE if all listed variables are TRUE.
-find_package_handle_standard_args(
-  CPLEX_ILOCPLEX DEFAULT_MSG
-  CPLEX_ILOCPLEX_LIBRARY CPLEX_ILOCPLEX_LIBRARY_DEBUG
-  CPLEX_ILOCPLEX_INCLUDE_DIR CPLEX_FOUND CPLEX_CONCERT_FOUND)
+# Handle the QUIETLY and REQUIRED arguments and set CPLEX_ILOCPLEX_FOUND to TRUE if all listed variables are TRUE.
+find_package_handle_standard_args(CPLEX_ILOCPLEX
+                                  DEFAULT_MSG
+                                  CPLEX_ILOCPLEX_LIBRARY
+                                  CPLEX_ILOCPLEX_LIBRARY_DEBUG
+                                  CPLEX_ILOCPLEX_INCLUDE_DIR
+                                  CPLEX_FOUND
+                                  CPLEX_CONCERT_FOUND)
 
-mark_as_advanced(CPLEX_ILOCPLEX_LIBRARY CPLEX_ILOCPLEX_LIBRARY_DEBUG
-  CPLEX_ILOCPLEX_INCLUDE_DIR)
+mark_as_advanced(CPLEX_ILOCPLEX_LIBRARY CPLEX_ILOCPLEX_LIBRARY_DEBUG CPLEX_ILOCPLEX_INCLUDE_DIR)
--- misc/FindGurobi.cmake
+++ misc/FindGurobi.cmake
@@ -1,52 +1,109 @@
-# Try to find the Gurobi libraries.
-# Modified from https://github.com/ampl/mp/blob/master/support/cmake/FindCPLEX.cmake
+# Try to find the Gurobi libraries. Modified from https://github.com/ampl/mp/blob/master/support/cmake/FindCPLEX.cmake
 
 include(FindPackageHandleStandardArgs)
 
-# Find the path to the main Gurobi folder.
-# Gurobi can be installed in the following default locations:
-#   /opt/gurobi<version>/linux64 - Linux
-#   /Library/gurobi<version>/mac64 - Mac OS X
-#   C:\gurobi<version>\win64 - Windows
+# Find the path to the main Gurobi folder. Gurobi can be installed in the following default locations:
+# /opt/gurobi<version>/linux64 - Linux /Library/gurobi<version>/mac64 - Mac OS X C:\gurobi<version>\win64 - Windows
 
-file(GLOB GUROBI_SEARCH_PATHS ${GUROBI_DIR}	"/opt/gurobi/gurobi*/linux64" "/opt/gurobi*/linux64" "/Library/gurobi*/mac64" "C:\\gurobi*\\win64")
+file(GLOB
+     GUROBI_SEARCH_PATHS
+     ${GUROBI_DIR}
+     "/opt/gurobi/gurobi*/linux64"
+     "/opt/gurobi*/linux64"
+     "/Library/gurobi*/mac64"
+     "C:\\gurobi*\\win64")
 find_path(GUROBI_INSTALL_DIR NAMES "include/gurobi_c++.h" PATHS ${GUROBI_SEARCH_PATHS})
 message(STATUS "Found Gurobi folder: ${GUROBI_INSTALL_DIR}")
 
 if(GUROBI_INSTALL_DIR)
-	set(GUROBI_INCLUDE_DIR "${GUROBI_INSTALL_DIR}/include")
-	set(GUROBI_LIB_DIR "${GUROBI_INSTALL_DIR}/lib")
-	message(STATUS "Using Gurobi include folder: ${GUROBI_INCLUDE_DIR}")
-	message(STATUS "Using Gurobi library folder: ${GUROBI_LIB_DIR}")
+  set(GUROBI_INCLUDE_DIR "${GUROBI_INSTALL_DIR}/include")
+  set(GUROBI_LIB_DIR "${GUROBI_INSTALL_DIR}/lib")
+  message(STATUS "Using Gurobi include folder: ${GUROBI_INCLUDE_DIR}")
+  message(STATUS "Using Gurobi library folder: ${GUROBI_LIB_DIR}")
 
-	if(UNIX)
-		file(GLOB GUROBI_LIBRARY ${GUROBI_LIB_DIR}/libgurobi*.so)
-		file(GLOB GUROBI_LIBRARY_DEBUG ${GUROBI_LIB_DIR}/libgurobi*.so)
-		file(GLOB GUROBI_CPP_LIBRARY ${GUROBI_LIB_DIR}/libgurobi_g++5.2.a)
+  if(UNIX)
+    file(GLOB GUROBI_LIBRARY ${GUROBI_LIB_DIR}/libgurobi*.so)
+    file(GLOB GUROBI_LIBRARY_DEBUG ${GUROBI_LIB_DIR}/libgurobi*.so)
 
-	elseif(APPLE)
-		file(GLOB GUROBI_LIBRARY ${GUROBI_LIB_DIR}/libgurobi*.so)
-		file(GLOB GUROBI_LIBRARY_DEBUG ${GUROBI_LIB_DIR}/libgurobi*.so)
-		file(GLOB GUROBI_CPP_LIBRARY ${GUROBI_LIB_DIR}/libgurobi_g++5.2.a)
+    list(FILTER
+         GUROBI_LIBRARY
+         EXCLUDE
+         REGEX
+         ".*_light.so$")
+    list(FILTER
+         GUROBI_LIBRARY_DEBUG
+         EXCLUDE
+         REGEX
+         ".*_light.so$")
 
-	else()
-		if(NOT (MSVC_VERSION LESS 1910))
-			file(GLOB GUROBI_LIBRARY ${GUROBI_LIB_DIR}/gurobi_c++md2017.lib)
-			file(GLOB GUROBI_LIBRARY_DEBUG ${GUROBI_LIB_DIR}/gurobi_c++mdd2017.lib)
-			message(STATUS "Found Gurobi library: ${GUROBI_LIBRARY}")
-		elseif (NOT (MSVC_VERSION LESS 1900)) 
-			file(GLOB GUROBI_LIBRARY ${GUROBI_LIB_DIR}/gurobi_c++md2015.lib)
-			file(GLOB GUROBI_LIBRARY_DEBUG ${GUROBI_LIB_DIR}/gurobi_c++mdd2015.lib)
-		endif ()
+    if(CMAKE_COMPILER_IS_GNUCXX)
+      execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
+      if(GCC_VERSION VERSION_GREATER 5.2 OR GCC_VERSION VERSION_EQUAL 5.2)
+        file(GLOB GUROBI_CPP_LIBRARY ${GUROBI_LIB_DIR}/libgurobi_g++5.2.a)
+      else()
+        file(GLOB GUROBI_CPP_LIBRARY ${GUROBI_LIB_DIR}/libgurobi_g++4.2.a)
+      endif()
 
-		file(GLOB GUROBI_CPP_LIBRARY ${GUROBI_LIB_DIR}/gurobi*.lib)
-		list(FILTER GUROBI_CPP_LIBRARY EXCLUDE REGEX "gurobi_.*lib$")
-	endif()
-	
-	message(STATUS "Found Gurobi library: ${GUROBI_LIBRARY}")
-	message(STATUS "Found Gurobi C++ library: ${GUROBI_CPP_LIBRARY}")
+      if(NOT (GUROBI_CPP_LIBRARY))
+        file(GLOB GUROBI_CPP_LIBRARY ${GUROBI_LIB_DIR}/libgurobi_c++.a)
+      endif()
+    endif()
+
+    list(FILTER
+         GUROBI_LIBRARY
+         EXCLUDE
+         REGEX
+         ".*_light.so$")
+    list(FILTER
+         GUROBI_LIBRARY_DEBUG
+         EXCLUDE
+         REGEX
+         ".*_light.so$")
+
+  elseif(APPLE)
+    file(GLOB GUROBI_LIBRARY ${GUROBI_LIB_DIR}/libgurobi*.so)
+    file(GLOB GUROBI_LIBRARY_DEBUG ${GUROBI_LIB_DIR}/libgurobi*.so)
+
+    if(CMAKE_COMPILER_IS_GNUCXX)
+      execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
+      if(GCC_VERSION VERSION_GREATER 5.2 OR GCC_VERSION VERSION_EQUAL 5.2)
+        file(GLOB GUROBI_CPP_LIBRARY ${GUROBI_LIB_DIR}/libgurobi_g++5.2.a)
+      else()
+        file(GLOB GUROBI_CPP_LIBRARY ${GUROBI_LIB_DIR}/libgurobi_g++4.2.a)
+      endif()
+
+      if(NOT (GUROBI_CPP_LIBRARY))
+        file(GLOB GUROBI_CPP_LIBRARY ${GUROBI_LIB_DIR}/libgurobi_c++.a)
+      endif()
+    endif()
+
+  else()
+    if(NOT (MSVC_VERSION LESS 1910))
+      file(GLOB GUROBI_LIBRARY ${GUROBI_LIB_DIR}/gurobi_c++md2017.lib)
+      file(GLOB GUROBI_LIBRARY_DEBUG ${GUROBI_LIB_DIR}/gurobi_c++mdd2017.lib)
+    elseif(NOT (MSVC_VERSION LESS 1900))
+      file(GLOB GUROBI_LIBRARY ${GUROBI_LIB_DIR}/gurobi_c++md2015.lib)
+      file(GLOB GUROBI_LIBRARY_DEBUG ${GUROBI_LIB_DIR}/gurobi_c++mdd2015.lib)
+    endif()
+
+    file(GLOB GUROBI_CPP_LIBRARY ${GUROBI_LIB_DIR}/gurobi*.lib)
+    list(FILTER
+         GUROBI_CPP_LIBRARY
+         EXCLUDE
+         REGEX
+         "gurobi_.*lib$")
+  endif()
+
+  message(STATUS "Found Gurobi library: ${GUROBI_LIBRARY}")
+  message(STATUS "Found Gurobi C++ library: ${GUROBI_CPP_LIBRARY}")
 endif()
 
 # Handle the QUIETLY and REQUIRED arguments and set GUROBI_FOUND to TRUE if all listed variables are TRUE.
-find_package_handle_standard_args(GUROBI DEFAULT_MSG GUROBI_LIBRARY GUROBI_CPP_LIBRARY)
-mark_as_advanced(GUROBI_LIBRARY GUROBI_CPP_LIBRARY GUROBI_INCLUDE_DIR GUROBI_LIB_DIR)
+find_package_handle_standard_args(GUROBI
+                                  DEFAULT_MSG
+                                  GUROBI_LIBRARY
+                                  GUROBI_CPP_LIBRARY)
+mark_as_advanced(GUROBI_LIBRARY
+                 GUROBI_CPP_LIBRARY
+                 GUROBI_INCLUDE_DIR
+                 GUROBI_LIB_DIR)
