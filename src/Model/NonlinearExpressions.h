813181ca82cc2a25fe16328a122a81a06934e198
Andreas Lundell <andreas.lundell@abo.fi>
2021-06-14 15:55:00 +0300

    Fix for convexity identification for square roots

--- src/Model/NonlinearExpressions.h
+++ src/Model/NonlinearExpressions.h
@@ -595,37 +595,36 @@ public:
 
     inline E_Convexity getConvexity() const override
     {
+        NonlinearExpressions children;
+        auto isValid = true;
+
+        if(child->getNumberOfChildren() == 1)
+        {
+            children.add(std::dynamic_pointer_cast<ExpressionUnary>(child)->child);
+        }
+        else if(child->getType() == E_NonlinearExpressionTypes::Sum)
+        {
+            children = std::dynamic_pointer_cast<ExpressionGeneral>(child)->children;
+        }
+        else
+        {
+            isValid = false;
+        }
+
+        for(auto& C : children)
+        {
+            if(!(C->getBounds().l() >= 0 && (C->getConvexity() <= E_Convexity::Convex)))
+            {
+                isValid = false;
+                break;
+            }
+        }
+
+        if(isValid)
+            return E_Convexity::Convex;
+
         auto childConvexity = child->getConvexity();
         auto childBounds = child->getBounds();
-
-        // The square root of sums of nonnegative convex quadratic functions is convex
-        if(child->getType() == E_NonlinearExpressionTypes::Sum)
-        {
-            auto sumExpr = std::dynamic_pointer_cast<ExpressionGeneral>(child);
-
-            auto isValid = true;
-
-            for(auto& C : sumExpr->children)
-            {
-                if(C->getType() == E_NonlinearExpressionTypes::Square)
-                {
-                    if(!(C->getBounds().l() >= 0
-                           && (C->getConvexity() == E_Convexity::Linear || C->getConvexity() == E_Convexity::Convex)))
-                    {
-                        isValid = false;
-                        break;
-                    }
-                }
-                else
-                {
-                    isValid = false;
-                    break;
-                }
-            }
-
-            if(isValid)
-                return E_Convexity::Convex;
-        }
 
         if(childBounds.l() >= 0 && childConvexity == E_Convexity::Concave)
             return E_Convexity::Concave;
@@ -2553,6 +2552,9 @@ public:
 
         if(numberOfChildren == 0)
             return E_Convexity::Unknown;
+
+        if(numberOfChildren == 1)
+            return children.at(0)->getConvexity();
 
         if(numberOfChildren == 2)
         {
