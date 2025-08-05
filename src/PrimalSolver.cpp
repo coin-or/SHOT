670c92246f474adc07d906e359d5bb455eeb1452
andreaslundell <andreas.lundell@abo.fi>
2025-08-05 11:13:29 +0300

    Fix typo

--- src/PrimalSolver.cpp
+++ src/PrimalSolver.cpp
@@ -137,7 +137,7 @@ bool PrimalSolver::checkPrimalSolutionPoint(PrimalSolution primalSol)
 
     primalSol.sourceDescription = sourceDesc;
 
-    // Recalculate if the objective to be sure it is correct
+    // Recalculate the objective to be sure it is correct
     primalSol.objValue = env->problem->objectiveFunction->calculateValue(primalSol.point);
     tmpObjVal = primalSol.objValue;
 
